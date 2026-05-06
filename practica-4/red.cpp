07939d2355cc8198fda3960b0f1d5823d84e1345
Juan Jose Ortega <juan.omesias@udea.edu.co>
2026-05-06 12:52:05 -0500

    muetra la red y la dijkstra y cambios en erroes del codigo

--- practica-4/red.cpp
+++ practica-4/red.cpp
@@ -1,4 +1,5 @@
 #include "red.h"
+#include <iostream>
 
 void Red::agregarRouter(string id) {
     routers.insert({id, Router(id)});
@@ -11,6 +12,7 @@ void Red::eliminarRouter(string id) {
         par.second.eliminarVecino(id);
     }
 }
+
 void Red::conectarRouters(string id1, string id2, int costo) {
     routers[id1].agregarVecino(id2, costo);
     routers[id2].agregarVecino(id1, costo);
@@ -19,4 +21,53 @@ void Red::conectarRouters(string id1, string id2, int costo) {
 void Red::eliminarConexion(string id1, string id2) {
     routers[id1].eliminarVecino(id2);
     routers[id2].eliminarVecino(id1);
+}
+
+void Red::mostrarRed() {
+    for (auto &par : routers) {
+        par.second.imprimirVecinos();
+    }
+}
+
+map<string, int> Red::dijkstra(string origen) {
+    map<string, int> distancias;
+    map<string, bool> visitado;
+
+    for (auto &par : routers) {
+        distancias[par.first] = numeric_limits<int>::max();
+        visitado[par.first] = false;
+    }
+
+    distancias[origen] = 0;
+
+
+    for (size_t i = 0; i < routers.size(); i++) {
+        string actual;
+        int minDist = numeric_limits<int>::max();
+
+        //Busca distnacia menor
+        for (auto &par : distancias) {
+            if (!visitado[par.first] && par.second < minDist) {
+                minDist = par.second;
+                actual = par.first;
+            }
+        }
+
+
+        if (minDist == numeric_limits<int>::max()) break;
+
+        visitado[actual] = true;
+
+
+        for (auto &vecino : routers[actual].getVecinos()) {
+            string nombre = vecino.first;
+            int costo = vecino.second;
+
+            if (distancias[actual] + costo < distancias[nombre]) {
+                distancias[nombre] = distancias[actual] + costo;
+            }
+        }
+    }
+
+    return distancias;
 }
--- practica-4/red.h
+++ practica-4/red.h
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <vector>
+#include <limits>
 #include "Router.h"
 
 using namespace std;
@@ -12,10 +14,19 @@ private:
     map<string, Router> routers;
 
 public:
+    //Se gestionara el router
     void agregarRouter(string id);
     void eliminarRouter(string id);
+
+    //control de Conexiones
     void conectarRouters(string id1, string id2, int costo);
     void eliminarConexion(string id1, string id2);
+
+    // Visualización de la red
+    void mostrarRed();
+
+    // Algoritmo de caminos
+    map<string, int> dijkstra(string origen);
 };
 
 #endif
