8c5832e8fcffd6073cb17d196595d8859336344d
Brayan Daniel Salazar <brayan.salazar1@udea.edu.co>
2026-06-04 00:17:47 -0500

    primera version modulo fisica

--- practica5-2/fisica.cpp
+++ practica5-2/fisica.cpp
@@ -1,3 +1,78 @@
 #include "fisica.h"
 
-fisica::fisica() {}
+Fisica::Fisica()
+{
+    factorDanio = 0.05;
+}
+
+void Fisica::agregarProyectil(
+    Proyectil *p)
+{
+    proyectiles.push_back(p);
+}
+
+void Fisica::agregarInfraestructura(
+    Infraestructura *i)
+{
+    infraestructuras.push_back(i);
+}
+
+double Fisica::calcularDanio(
+    Proyectil *p)
+{
+    return factorDanio *
+           p->getMasa() *
+           p->getVelocidad();
+}
+
+void Fisica::actualizarSistema(
+    double dt,
+    double ancho,
+    double alto)
+{
+    for(auto p : proyectiles)
+    {
+        p->actualizar(dt);
+
+        if(p->getX() <= 0 ||
+            p->getX() >= ancho)
+        {
+            p->reboteHorizontal();
+        }
+
+        if(p->getY() <= 0 ||
+            p->getY() >= alto)
+        {
+            p->reboteVertical(1.0);
+        }
+
+        for(auto i : infraestructuras)
+        {
+            if(i->detectarColision(
+                    p->getX(),
+                    p->getY()))
+            {
+                double danio =
+                    calcularDanio(p);
+
+                i->recibirDanio(
+                    danio);
+
+                p->reboteVertical(
+                    i->getRestitucion());
+            }
+        }
+    }
+}
+
+std::vector<Proyectil*>&
+Fisica::getProyectiles()
+{
+    return proyectiles;
+}
+
+std::vector<Infraestructura*>&
+Fisica::getInfraestructuras()
+{
+    return infraestructuras;
+}
--- practica5-2/fisica.h
+++ practica5-2/fisica.h
@@ -1,10 +1,44 @@
 #ifndef FISICA_H
 #define FISICA_H
 
-class fisica
+#include <vector>
+
+#include "proyectil.h"
+#include "infraestructura.h"
+
+class Fisica
 {
+private:
+
+    std::vector<Proyectil*> proyectiles;
+
+    std::vector<Infraestructura*> infraestructuras;
+
+    double factorDanio;
+
 public:
-    fisica();
+
+    Fisica();
+
+    void agregarProyectil(
+        Proyectil* p);
+
+    void agregarInfraestructura(
+        Infraestructura* i);
+
+    void actualizarSistema(
+        double dt,
+        double ancho,
+        double alto);
+
+    double calcularDanio(
+        Proyectil* p);
+
+    std::vector<Proyectil*>&
+    getProyectiles();
+
+    std::vector<Infraestructura*>&
+    getInfraestructuras();
 };
 
-#endif // FISICA_H
+#endif
