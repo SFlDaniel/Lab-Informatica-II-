21e4655c0bcb2c823df4683b593157f0cb75e543
Brayan Daniel Salazar <brayan.salazar1@udea.edu.co>
2026-06-03 23:22:48 -0500

    modulo infraestructura

--- practica5-2/infraestructura.cpp
+++ practica5-2/infraestructura.cpp
@@ -1,3 +1,78 @@
 #include "infraestructura.h"
 
-infraestructura::infraestructura() {}
+Infraestructura::Infraestructura(
+    double xIn,
+    double yIn,
+    double anchoIn,
+    double altoIn,
+    double resistenciaIn,
+    double restitucionIn)
+{
+    x = xIn;
+    y = yIn;
+
+    ancho = anchoIn;
+    alto = altoIn;
+
+    resistencia = resistenciaIn;
+
+    restitucion = restitucionIn;
+}
+
+bool Infraestructura::detectarColision(
+    double px,
+    double py)
+{
+    return (
+        px >= x &&
+        px <= x + ancho &&
+        py >= y &&
+        py <= y + alto
+        );
+}
+
+void Infraestructura::recibirDanio(
+    double danio)
+{
+    resistencia -= danio;
+
+    if(resistencia < 0)
+    {
+        resistencia = 0;
+    }
+}
+
+bool Infraestructura::destruido()
+{
+    return resistencia <= 0;
+}
+
+double Infraestructura::getX()
+{
+    return x;
+}
+
+double Infraestructura::getY()
+{
+    return y;
+}
+
+double Infraestructura::getAncho()
+{
+    return ancho;
+}
+
+double Infraestructura::getAlto()
+{
+    return alto;
+}
+
+double Infraestructura::getResistencia()
+{
+    return resistencia;
+}
+
+double Infraestructura::getRestitucion()
+{
+    return restitucion;
+}
--- practica5-2/infraestructura.h
+++ practica5-2/infraestructura.h
@@ -1,10 +1,43 @@
 #ifndef INFRAESTRUCTURA_H
 #define INFRAESTRUCTURA_H
 
-class infraestructura
+class Infraestructura
 {
+private:
+
+    double x;
+    double y;
+
+    double ancho;
+    double alto;
+
+    double resistencia;
+
+    double restitucion;
 public:
-    infraestructura();
+    Infraestructura(double x,
+                    double y,
+                    double ancho,
+                    double alto,
+                    double resistencia,
+                    double restitucion);
+
+    bool detectarColision(double px,
+                          double py);
+
+    void recibirDanio(double danio);
+
+    bool destruido();
+
+    double getX();
+    double getY();
+
+    double getAncho();
+    double getAlto();
+
+    double getResistencia();
+
+    double getRestitucion();
 };
 
 #endif // INFRAESTRUCTURA_H
